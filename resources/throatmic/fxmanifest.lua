17092a93c1ca8099969e7b4bb078c51ed49b1934
Javier143322 <huhdnuhdl@gmail.com>
2025-10-17 12:07:06 -0500

    Actualizar fxmanifest.lua

--- resources/throatmic/fxmanifest.lua
+++ resources/throatmic/fxmanifest.lua
@@ -1,1 +1,29 @@
 
+fx_version 'cerulean'
+game 'gta5'
+
+author 'TuServidor'
+description 'Sistema Throat Mic Pro - Comunicación Táctica'
+version '3.0.0'
+
+ui_page 'html/throatmic.html'
+
+client_scripts {
+    'client.lua'
+}
+
+server_scripts {
+    '@mysql-async/lib/MySQL.lua',
+    'server.lua'
+}
+
+files {
+    'html/throatmic.html',
+    'html/style.css',
+    'html/script.js'
+}
+
+dependencies {
+    'es_extended',
+    'pma-voice'
+}
